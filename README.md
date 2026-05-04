f42afb354f43eea51d139a84118eece62e4ffaa5
jjosko <01187232@pw.edu.pl>
2026-05-04 10:16:16 +0200

    added basic exaple parser

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# PF-26L-parser
+# USAGE
+to run the program use cabal and install dependencies listed in 'parser.cabal' (running 'cabal install' while in the root directory of the project should be enough). To execute run 'cabal run'.
+
+
+# Revision history for myfirstapp
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,49 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+import Data.Void
+import qualified Data.Text as T
+import Data.Text (Text)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+import qualified Data.Text.IO()
+
+type Parser = Parsec Void Text
+
+data Expr
+  = Number Int
+  | Var String
+  | Assign String Expr
+  deriving (Show)
+
+pr :: Parser ()
+pr = L.space space1 empty empty
+
+helper :: Parser a -> Parser a
+helper = L.lexeme pr
+
+symbol :: Text -> Parser Text
+symbol = L.symbol pr
+
+number :: Parser Expr
+number = Number <$> helper L.decimal
+
+identifier :: Parser String
+identifier = helper ((:) <$> letterChar <*> many alphaNumChar)
+variable :: Parser Expr
+variable = Var <$> identifier
+
+assign :: Parser Expr
+assign = do
+  name <- identifier
+  symbol "="
+  expr <- number <|> variable
+  return $ Assign name expr
+main :: IO()
+main = do
+    let input = "x = 6"
+    case parse assign "" (T.pack input) of
+        Left err -> putStrLn (errorBundlePretty err)
+        Right result -> print result
